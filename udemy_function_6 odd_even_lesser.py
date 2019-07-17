e0352a85a5085f4c11d9f508ecdfae50d38ee835
Michal Borkowski <44318554+mike1806@users.noreply.github.com>
2019-07-17 20:14:17 +0100

    Add files via upload

---
+++ udemy_function_6 odd_even_lesser.py
@@ -0,0 +1,21 @@
+'''
+Lesser of two evens: Write a function that returns the lesser of two given numbers if both numbers are even,
+but returns the greater if one or both numbers are odd
+
+'''
+
+def lesser_of_two_evens(a,b):
+    if a%2 == 0 and b%2 == 0:
+        #both numbers are even!
+        if a < b:
+            result = a
+        else:
+            result = b
+    else:
+        # one or both numbers are odd!
+        if a > b
+            result = a
+        else:
+            result = b
+
+    return result
