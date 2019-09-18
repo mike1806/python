4ed65e67d611d710244269d61bd5607013807ccf
Michal Borkowski <44318554+mike1806@users.noreply.github.com>
2019-09-18 21:27:22 +0100

    Add files via upload

---
+++ firstClass.py
@@ -0,0 +1,21 @@
+import sys
+
+def main():
+    print"Hello world"
+
+if __name__=='__main__':
+    main()
+
+to see the content
+dir(sys)
+help(sys.argv)
+
+# the first python method
+
+def foo(a, b, c):
+    x = bar()
+    if x < 3:
+        return 9
+    return x+4
+
+
